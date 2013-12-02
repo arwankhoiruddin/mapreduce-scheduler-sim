9cd7743b36315c5a8e694e7b11c6d43ea3038505
Pradeeban <pradeeban@llovizna.(none)>
2013-12-02 22:38:43 +0100

    Moving Hosts class.

--- examples/other/Hosts.java
+++ examples/other/Hosts.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.cloudbus.cloudsim.examples.roundrobin;
+package org.cloudbus.cloudsim.examples.other;
 
 import java.util.ArrayList;
 import java.util.Collections;
