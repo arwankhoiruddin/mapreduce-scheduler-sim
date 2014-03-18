41ef1de856a255791351bc4996006821aef1fc8c
pradeeban <kk.pradeeban@gmail.com>
2014-03-18 13:02:54 +0000

    license header.

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/cloud2sim/main/Simulator.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/cloud2sim/main/Simulator.java
@@ -1,10 +1,11 @@
 /*
- * Title:        CloudSim Toolkit
- * Description:  CloudSim (Cloud Simulation) Toolkit for Modeling and Simulation
- *               of Clouds
+ * Title:        Cloud2Sim
+ * Description:  Distributed and Concurrent Cloud Simulation
+ *               Toolkit for Modeling and Simulation
+ *               of Clouds - Enhanced version of CloudSim.
  * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
  *
- * Copyright (c) 2009, The University of Melbourne, Australia
+ * Copyright (c) 2014, Pradeeban Kathiravelu <pradeeban.kathiravelu@tecnico.ulisboa.pt>
  */
 
 package org.cloudbus.cloudsim.examples.cloud2sim.main;
