587054c04bdc9a42dde6d22934a5292dde9ec6ce
pradeeban <kk.pradeeban@gmail.com>
2014-03-25 18:52:16 +0000

    wait for initiation.

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/hazelcast/HazelSim.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/hazelcast/HazelSim.java
@@ -28,6 +28,17 @@ public class HazelSim extends HazelSimCore {
         return true;
     }
 
+    private static void waitForInitiation() {
+        if (instances[HazelSimConstants.FIRST] == null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     private HazelSim() {
     }
 
@@ -72,7 +83,7 @@ public class HazelSim extends HazelSimCore {
      * @return the vm list
      */
     public IMap<Integer, Vm> getUserVmList() {
-//        waitForInitiation();
+        waitForInitiation();
         return instances[HazelSimConstants.LAST].getMap("userVmList");
     }
 
@@ -82,7 +93,7 @@ public class HazelSim extends HazelSimCore {
      * @return the cloudlet map
      */
     public IMap<Integer, Cloudlet> getUserCloudletList() {
-//        waitForInitiation();
+        waitForInitiation();
         return instances[HazelSimConstants.FIRST].getMap("userCloudletList");
     }
 
