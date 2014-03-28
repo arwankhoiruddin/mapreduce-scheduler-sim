88be0325cdd8b546a0a2dc60e11e785ff6f0497a
pradeeban <kk.pradeeban@gmail.com>
2014-03-28 22:37:45 +0000

    License Header

--- modules/cloud2sim/src/main/java/pt/inesc_id/gsd/cloud2sim/autoscale/HealthMonitor.java
+++ modules/cloud2sim/src/main/java/pt/inesc_id/gsd/cloud2sim/autoscale/HealthMonitor.java
@@ -1,3 +1,13 @@
+/*
+ * Title:        Cloud2Sim
+ * Description:  Distributed and Concurrent Cloud Simulation
+ *               Toolkit for Modeling and Simulation
+ *               of Clouds - Enhanced version of CloudSim.
+ * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * Copyright (c) 2014, Pradeeban Kathiravelu <pradeeban.kathiravelu@tecnico.ulisboa.pt>
+ */
+
 package pt.inesc_id.gsd.cloud2sim.autoscale;
 
 import java.lang.management.ManagementFactory;
