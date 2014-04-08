f2acc769c1a20e722f1229e22c570d28ab06d5a8
pradeeban <kk.pradeeban@gmail.com>
2014-04-08 13:47:15 +0100

    Adding the map-reduce example (thanks to Hazelcast community for the example!) of Hazelcast into cloud2sim.
    TODO: Generify for the general simulations.

---
+++ modules/cloud2sim/src/main/java/pt/inesc_id/gsd/cloud2sim/mapreduce/MapReduceSimulator.java
@@ -0,0 +1,148 @@
+/*
+ * Title:        Cloud2Sim
+ * Description:  Distributed and Concurrent Cloud Simulation
+ *               Toolkit for Modeling and Simulation
+ *               of Clouds - Enhanced version of CloudSim.
+ * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * Copyright (c) 2014, Pradeeban Kathiravelu <pradeeban.kathiravelu@tecnico.ulisboa.pt>
+ */
+
+package pt.inesc_id.gsd.cloud2sim.mapreduce;
+
+import com.hazelcast.core.ExecutionCallback;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICompletableFuture;
+import com.hazelcast.core.IMap;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobTracker;
+import com.hazelcast.mapreduce.KeyValueSource;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.compatibility.ConfigReader;
+import pt.inesc_id.gsd.cloud2sim.hazelcast.HzCloudSim;
+import pt.inesc_id.gsd.cloud2sim.hazelcast.HzObjectCollection;
+import pt.inesc_id.gsd.cloud2sim.util.AppUtil;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.Calendar;
+import java.util.Map;
+
+public class MapReduceSimulator {
+    private static final String[] DATA_RESOURCES_TO_LOAD = {"text1.txt", "text2.txt", "text3.txt"};
+
+    public static void main(String[] args) throws Exception {
+        AppUtil.start();
+        Log.printLine("# Starting the Map Reduce Simulator...");
+
+        Calendar calendar = Calendar.getInstance();
+        boolean trace_flag = false;  // mean trace events
+
+        // Initialize the CloudSim library
+        HzCloudSim.init(ConfigReader.getNoOfUsers(), calendar, trace_flag);
+
+        HazelcastInstance hazelcastInstance = HzObjectCollection.getHzObjectCollection().getFirstInstance();
+
+        try {
+            fillMapWithData(hazelcastInstance);
+
+            Map<String, Long> countsPerWord = mapReduce(hazelcastInstance);
+            System.out.println("Counts per words over " + DATA_RESOURCES_TO_LOAD.length + " files:");
+            for (Map.Entry<String, Long> entry : countsPerWord.entrySet()) {
+                System.out.println("\tWord '" + entry.getKey() + "' occured " + entry.getValue() + " times");
+            }
+
+            long wordCount = mapReduceCollate(hazelcastInstance);
+            System.out.println("All content sums up to " + wordCount + " words.");
+
+        } finally {
+            Hazelcast.shutdownAll();
+        }
+    }
+
+    private static Map<String, Long> mapReduce(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        // Retrieving the JobTracker by name
+        JobTracker jobTracker = hazelcastInstance.getJobTracker("default");
+
+        // Creating the KeyValueSource for a Hazelcast IMap
+        IMap<String, String> map = hazelcastInstance.getMap("articles");
+        KeyValueSource<String, String> source = KeyValueSource.fromMap(map);
+
+        Job<String, String> job = jobTracker.newJob(source);
+
+        // Creating a new Job
+        ICompletableFuture<Map<String, Long>> future = job // returned future
+                .mapper(new TokenizerMapper())             // adding a mapper
+                .combiner(new WordCountCombinerFactory())  // adding a combiner through the factory
+                .reducer(new WordCountReducerFactory())    // adding a reducer through the factory
+                .submit();                                 // submit the task
+
+        // Attach a callback listener
+        future.andThen(buildCallback());
+
+        // Wait and retrieve the result
+        return future.get();
+    }
+
+    private static long mapReduceCollate(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        // Retrieving the JobTracker by name
+        JobTracker jobTracker = hazelcastInstance.getJobTracker("default");
+
+        // Creating the KeyValueSource for a Hazelcast IMap
+        IMap<String, String> map = hazelcastInstance.getMap("articles");
+        KeyValueSource<String, String> source = KeyValueSource.fromMap(map);
+
+        // Creating a new Job
+        Job<String, String> job = jobTracker.newJob(source);
+
+        ICompletableFuture<Long> future = job // returned future
+                .mapper(new TokenizerMapper())             // adding a mapper
+                .combiner(new WordCountCombinerFactory())  // adding a combiner through the factory
+                .reducer(new WordCountReducerFactory())    // adding a reducer through the factory
+                .submit(new WordCountCollator());          // submit the task and supply a collator
+
+        // Wait and retrieve the result
+        return future.get();
+    }
+
+    private static ExecutionCallback<Map<String, Long>> buildCallback() {
+        return new ExecutionCallback<Map<String, Long>>() {
+            @Override
+            public void onResponse(Map<String, Long> stringLongMap) {
+                System.out.println("Calculation finished! :)");
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        };
+    }
+
+    private static void fillMapWithData(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        IMap<String, String> map = hazelcastInstance.getMap("articles");
+        for (String file : DATA_RESOURCES_TO_LOAD) {
+            InputStream is = MapReduceSimulator.class.getResourceAsStream("conf/" + file);
+            LineNumberReader reader = new LineNumberReader(new InputStreamReader(is));
+
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            map.put(file, sb.toString());
+
+            is.close();
+            reader.close();
+        }
+    }
+
+}
---
+++ modules/cloud2sim/src/main/java/pt/inesc_id/gsd/cloud2sim/mapreduce/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *  A map-reduce implementation for Cloud2Sim, to abstract and simulate map-reduce applications.
+ *
+ */
+
+package pt.inesc_id.gsd.cloud2sim.mapreduce;
